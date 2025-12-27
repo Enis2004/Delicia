93e3642f35c2cac0aca341fa2a11a6445c8c33c0
Enis2004 <enis.merdzanovic@stu.ibu.edu.ba>
2025-12-27 16:08:10 +0100

    novipokusajv2

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -1,5 +1,5 @@
 let Constants = {
-    PROJECT_BASE_URL: "https://restoran-6zv4q.ondigitalocean.app/delicia-backend",
+    PROJECT_BASE_URL: "https://restoran-6zv4q.ondigitalocean.app/delicia-backend/",
     USER_ROLE: "user",
     ADMIN_ROLE: "admin"
  }
